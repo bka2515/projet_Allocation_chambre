184c80f826b876481fd35e563c2d63dd9bf6dabd
bka2515 <bka2515@gmail.com>
2020-06-25 01:10:24 +0200

    mon deusieme commit

---
+++ pages/chambres.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../style/stylechambres.css">
+  </head>
+  <body style=''>
+    <div class="row">
+  <button class="btn btn-primary ajout" id="ajout" type="button"> Ajouter chambre</button>
+</div>
+  <div class="row">
+  
+  <div class="col-lg-10 table">
+  <table class="">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Numéro chambre</th>
+      <th scope="col">Numéro bâtiment</th>
+      <th scope="col">Type</th>
+      <th scope="col">Paramètres</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">001</th>
+      <td>01</td>
+      <td>indiv</td>
+      <td><button class="btn-primary"><i class="fa fa-trash"></i></button>
+      <button class="btn-primary">
+      <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+      </button>
+    </td>
+    </tr>
+    <tr>
+      <th scope="row">003</th>
+      <td>03</td>
+      <td>a deux</td>
+      <td><button class="btn btn-primary"><i class="fa fa-trash"></i></button>
+      <button class="btn btn-primary">
+      <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+      </button>
+    </td>
+    </tr>
+    <tr>
+      <th scope="row">005</th>
+      <td>05</td>
+      <td>indiv</td>
+      <td><button class="btn btn-primary"><i class="fa fa-trash"></i></button>
+      <button class="btn btn-primary">
+      <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+      </button>
+    </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
